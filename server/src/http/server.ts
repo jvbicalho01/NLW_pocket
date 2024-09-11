c8adef0ca68b74d1deb99574d63853b1e39c8af8
João Vitor Bicalho <joaovitorbicalho.dasilva@gmail.com>
2024-09-11 10:52:13 -0300

    add route and query get pending goals

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,25 +1,48 @@
 import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
 import { createGoal } from "../functions/create-goal";
 import z from "zod";
+import { getWeekPendingGoals } from "../functions/get-week-pending-goals";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.post('/goals', async (request) => {
-  const createGoalSchema = z.object({
-    title: z.string(),
-    desiredWeeklyFrequency: z.number().min(1).max(7),
-  })
-  
-  const body = createGoalSchema.parse(request.body)
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-  await createGoal({
-    title: body.title,
-    desiredWeeklyFrequency: body.desiredWeeklyFrequency
-  })
-})
+app.get("/pending-goals", async () => {
+  const { pendingGoals } = await getWeekPendingGoals();
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log("HTTP server running!")
+  return { pendingGoals };
 });
+
+app.post(
+  "/goals",
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().min(1).max(7),
+      }),
+    },
+  },
+  async (request) => {
+    const { title, desiredWeeklyFrequency } = request.body;
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    });
+  }
+);
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP server running!");
+  });
