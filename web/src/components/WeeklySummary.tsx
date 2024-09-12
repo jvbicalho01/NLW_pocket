841fb3e1d1d870c5830287c8a6a26114df86005f
João Vitor Bicalho <joaovitorbicalho.dasilva@gmail.com>
2024-09-12 18:28:25 -0300

    add list goals page

---
+++ web/src/components/WeeklySummary.tsx
@@ -0,0 +1,123 @@
+import { CheckCircle2, Plus } from "lucide-react";
+import { Button } from "./ui/Button";
+import { DialogTrigger } from "./ui/Dialog";
+import { InOrbitIcon } from "./InOrbitIcon";
+import { Progress, ProgressIndicator } from "./ui/ProgressBar";
+import { Separator } from "./ui/Separator";
+import { OutlineButton } from "./ui/OutlineButton";
+
+export function WeeklySummary() {
+  return (
+    <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <InOrbitIcon />
+          <span className="text-lg font-semibold">5 a 10 de agosto</span>
+        </div>
+
+        <DialogTrigger asChild>
+          <Button size="sm">
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <Progress value={8} max={15}>
+          <ProgressIndicator
+            style={{
+              width: "50%",
+            }}
+          />
+        </Progress>
+
+        <div className="flex items-center justify-between text-xs text-zinc-400">
+          <span>
+            Você completou <span className="text-zinc-100">8</span> de{" "}
+            <span className="text-zinc-100">15</span> metas nessa semana.
+          </span>
+          <span>50%</span>
+        </div>
+      </div>
+
+      <Separator />
+
+      <div className="flex flex-wrap gap-3">
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Meditar
+        </OutlineButton>
+
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Nadar
+        </OutlineButton>
+
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Correr
+        </OutlineButton>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <h2 className="text-xl font-medium">Sua semana</h2>
+
+        <div className="flex flex-col gap-4">
+          <h3 className="font-medium">
+            Domingo{" "}
+            <span className="text-zinc-400 text-xs">(15 de setembro)</span>
+          </h3>
+
+          <ul className="flex flex-col gap-3">
+            <li className="flex items-center gap-2">
+              <CheckCircle2 className="size-4 text-pink-500" />
+              <span className="text-sm text-zinc-400">
+                Você completou{" "}
+                <span className="text-zinc-100">"Acordar cedo"</span> às{" "}
+                <span className="text-zinc-100">08:00</span>
+              </span>
+            </li>
+
+            <li className="flex items-center gap-2">
+              <CheckCircle2 className="size-4 text-pink-500" />
+              <span className="text-sm text-zinc-400">
+                Você completou{" "}
+                <span className="text-zinc-100">"Acordar cedo"</span> às{" "}
+                <span className="text-zinc-100">08:00</span>
+              </span>
+            </li>
+
+            <li className="flex items-center gap-2">
+              <CheckCircle2 className="size-4 text-pink-500" />
+              <span className="text-sm text-zinc-400">
+                Você completou{" "}
+                <span className="text-zinc-100">"Acordar cedo"</span> às{" "}
+                <span className="text-zinc-100">08:00</span>
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <h3 className="font-medium">
+            Segunda-feira{" "}
+            <span className="text-zinc-400 text-xs">(16 de setembro)</span>
+          </h3>
+
+          <ul className="flex flex-col gap-3">
+            <li className="flex items-center gap-2">
+              <CheckCircle2 className="size-4 text-pink-500" />
+              <span className="text-sm text-zinc-400">
+                Você completou{" "}
+                <span className="text-zinc-100">"Acordar cedo"</span> às{" "}
+                <span className="text-zinc-100">08:00</span>
+              </span>
+            </li>
+          </ul>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
